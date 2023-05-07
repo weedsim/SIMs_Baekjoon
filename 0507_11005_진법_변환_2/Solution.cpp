0c9cda75500f83901dfcad13eb8b6ab9c1282cb9
weedsim <109517699+weedsim@users.noreply.github.com>
2023-05-07 22:24:21 +0900

    0507_11005_브론즈_1

---
+++ 0507_11005_진법_변환_2/Solution.cpp
@@ -0,0 +1,43 @@
+// CRT_SECURE 에 대한 에러를 무시하는 용도이다. 간단한 알고리즘에서는 CRT_SECURE는 필요없기에 무시해도 된다.
+#define _CRT_SECURE_NO_WARNINGS
+// C++ 의 기본이다.
+#include <iostream> 
+// 이번에는 없어도 되지만 편의를 위해 존재하고 있다.
+#include <algorithm>
+// 출력은 string으로
+#include <string>
+// 일반적으로 std 를 붙여가면서 할 것을 미리 명시하는 방식으로 편의를 위해 존재한다.
+using namespace std;
+
+
+
+int main()
+{
+	// 아래 세 줄은 입력과 출력의 시간을 줄여주기위해 존재한다.
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, B;
+	cin >> N >> B;
+	string ans;
+	while (N != 0) {
+		int a;
+		a = N % B;
+		if (a > 9) {
+			string temp;
+			temp = a + 'A' - 10;
+			ans += temp;
+		}
+		else {
+			ans += to_string(a);
+		}
+		N /= B;
+	}
+
+	reverse(ans.begin(), ans.end());
+
+	cout << ans;
+
+	return 0;
+}
