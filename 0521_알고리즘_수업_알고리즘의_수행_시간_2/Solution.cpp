842895e064fab2b1eb050fad0aef4e4c747a17b0
weedsim <109517699+weedsim@users.noreply.github.com>
2023-05-22 00:00:26 +0900

    0521_브론즈_4

---
+++ 0521_알고리즘_수업_알고리즘의_수행_시간_2/Solution.cpp
@@ -0,0 +1,25 @@
+// CRT_SECURE 에 대한 에러를 무시하는 용도이다. 간단한 알고리즘에서는 CRT_SECURE는 필요없기에 무시해도 된다.
+#define _CRT_SECURE_NO_WARNINGS
+// C++ 의 기본이다.
+#include <iostream> 
+// 이번에는 없어도 되지만 편의를 위해 존재하고 있다.
+#include <algorithm>
+// 일반적으로 std 를 붙여가면서 할 것을 미리 명시하는 방식으로 편의를 위해 존재한다.
+using namespace std;
+
+
+
+int main()
+{
+	// 아래 세 줄은 입력과 출력의 시간을 줄여주기위해 존재한다.
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	cout << n << "\n" << 1;
+
+	return 0;
+}
