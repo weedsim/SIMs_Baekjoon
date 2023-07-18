1b8b630993df614e5525f00e2b5e36169bda50d2
weedsim <109517699+weedsim@users.noreply.github.com>
2023-07-18 23:27:11 +0900

    0718_브론즈_5

---
+++ 0718_4101_크냐/Solution.cpp
@@ -0,0 +1,37 @@
+// CRT_SECURE 에 대한 에러를 무시하는 용도이다. 간단한 알고리즘에서는 CRT_SECURE는 필요없기에 무시해도 된다.
+#define _CRT_SECURE_NO_WARNINGS
+// C++ 의 기본이다.
+#include <iostream> 
+// 이번에는 없어도 되지만 편의를 위해 존재하고 있다.
+#include <algorithm>
+// 일반적으로 std 를 붙여가면서 할 것을 미리 명시하는 방식으로 편의를 위해 존재한다.
+using namespace std;
+
+
+
+int main()
+{
+	// 아래 세 줄은 입력과 출력의 시간을 줄여주기위해 존재한다.
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int A, B;
+	
+	while (1) {
+		cin >> A >> B;
+		if (A == 0 && B == 0) { break; }
+
+		if (A > B) {
+			cout << "Yes";
+		}
+		else {
+			cout << "No";
+		}
+		cout << "\n";
+	}
+
+
+
+	return 0;
+}
