37446b95ce1aac3216e1ff3df1fd5950a91c803c
weedsim <109517699+weedsim@users.noreply.github.com>
2024-04-12 15:21:56 +0900

    [Gold V] Title: 제곱수 찾기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.cc
+++ 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
-#include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,22 +9,34 @@ bool isPerfectSquare(long long n) {
     return root * root == n;
 }
 
-long long findLargestSquare(const vector<string>& grid, int N, int M) {
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    int grid[10][10]; // 10x10 크기의 int 형 2차원 배열 선언
+
+    // 입력 처리
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            char c;
+            cin >> c;
+            grid[i][j] = c - '0'; // char를 int로 변환하여 저장
+        }
+    }
+
     long long largestSquare = -1;
 
-    for (int startX = 0; startX < N; ++startX) {
-        for (int startY = 0; startY < M; ++startY) {
-            for (int stepX = -N; stepX < N; ++stepX) {
-                for (int stepY = -M; stepY < M; ++stepY) {
+    for (int startX = 0; startX < N; startX++) {
+        for (int startY = 0; startY < M; startY++) {
+            for (int stepX = -N; stepX <= N; stepX++) {
+                for (int stepY = -M; stepY <= M; stepY++) {
                     if (stepX == 0 && stepY == 0) continue;
 
-                    string numStr = "";
+                    long long num = 0;
                     int x = startX, y = startY;
 
                     while (x >= 0 && x < N && y >= 0 && y < M) {
-                        numStr += grid[x][y];
-                        long long num = stoll(numStr);
-
+                        num = num * 10 + grid[x][y]; // 숫자 생성
                         if (isPerfectSquare(num)) {
                             largestSquare = max(largestSquare, num);
                         }
@@ -38,19 +49,7 @@ long long findLargestSquare(const vector<string>& grid, int N, int M) {
         }
     }
 
-    return largestSquare;
-}
-
-int main() {
-    int N, M;
-    cin >> N >> M;
-
-    vector<string> grid(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> grid[i];
-    }
-
-    cout << findLargestSquare(grid, N, M) << endl;
+    cout << largestSquare << endl;
 
     return 0;
 }
