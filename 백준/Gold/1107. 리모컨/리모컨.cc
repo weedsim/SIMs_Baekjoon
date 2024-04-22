a81bc3c84304f9d1b0557a8bc829191c09deef8a
weedsim <109517699+weedsim@users.noreply.github.com>
2024-04-22 15:34:30 +0900

    [Gold V] Title: 리모컨, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.cc
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -1,61 +1,67 @@
-#include <iostream>
-#include <vector>
+// CRT_SECURE 에 대한 에러를 무시하는 용도이다. 간단한 알고리즘에서는 CRT_SECURE는 필요없기에 무시해도 된다.
+#define _CRT_SECURE_NO_WARNINGS
+// C++ 의 기본이다.
+#include <iostream> 
+// 편의를 위해 존재하고 있다.
+#include <algorithm>
+// 계산용
 #include <cmath>
+#include <vector>
+// 일반적으로 std 를 붙여가면서 할 것을 미리 명시하는 방식으로 편의를 위해 존재한다.
 using namespace std;
 
-bool broken[10];
+bool broken[10] = { false, };
 
-// 해당 채널을 누를 수 있는지 없는지 확인하는 함수
-bool canPress(int channel) {
-    if (channel == 0) {
-        return !broken[0];
-    }
-    while (channel > 0) {
-        if (broken[channel % 10]) {
-            return false;
-        }
-        channel /= 10;
-    }
-    return true;
+bool isUsable(int num) {
+	if (num == 0) return !broken[0];
+	while (num > 0) {
+		if (broken[num % 10]) return false;
+		num /= 10;
+	}
+	return true;
 }
 
-// 채널 번호를 누르는데 필요한 버튼 누름 횟수를 반환하는 함수
-int pressCount(int channel) {
-    if (channel == 0) {
-        return 1;
-    }
-    int length = 0;
-    while (channel > 0) {
-        length++;
-        channel /= 10;
-    }
-    return length;
+int countPress(int num) {
+	if (num == 0) return 1;
+	int len = 0;
+	while (num > 0) {
+		len++;
+		num /= 10;
+	}
+	return len;
 }
 
-int main() {
-    int N, M;
-    cin >> N >> M;
-    for (int i = 0; i < M; i++) {
-        int button;
-        cin >> button;
-        broken[button] = true;
-    }
+int main()
+{
+	// 아래 세 줄은 입력과 출력의 시간을 줄여주기위해 존재한다.
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    // +, - 버튼만 사용하여 이동하는 경우의 초기값 설정
-    int result = abs(N - 100);
+	int N;
+	int M;
+	cin >> N;
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		int temp;
+		cin >> temp;
+		broken[temp] = true;
+	}
 
-    // 0부터 1000000까지 모든 채널을 확인
-    for (int i = 0; i <= 1000000; i++) {
-        // i 채널을 누를 수 있다면
-        if (canPress(i)) {
-            // i 채널을 누르는데 필요한 횟수 + i에서 N까지 + 혹은 -로 이동하는 횟수
-            int press = pressCount(i) + abs(N - i);
-            if (result > press) {
-                result = press;
-            }
-        }
-    }
+	if (N == 100) { cout << 0; return 0; }
 
-    cout << result;
-    return 0;
+	int ans = abs(N - 100);
+
+	for (int i = 0; i < 1000000; i++) {
+		
+		if (isUsable(i)) {
+			int pressCnt = countPress(i) + abs(N - i);
+			ans = min(ans, pressCnt);
+		}
+
+	}
+
+	cout << ans;
+
+	return 0;
 }
