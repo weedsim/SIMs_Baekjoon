48cc183580d521ec7769c1bbf187ce786ad34b8a
weedsim <109517699+weedsim@users.noreply.github.com>
2024-11-07 23:17:00 +0900

    [Bronze III] Title: 쿠폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10179. 쿠폰/쿠폰.cc
@@ -0,0 +1,38 @@
+// CRT_SECURE 에 대한 에러를 무시하는 용도이다. 간단한 알고리즘에서는 CRT_SECURE는 필요없기에 무시해도 된다.
+#define _CRT_SECURE_NO_WARNINGS
+// C++ 의 기본이다.
+#include <iostream> 
+// 편의를 위해 존재하고 있다.
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+// 임시 저장용도
+#include <vector>
+#include <queue>
+// 일반적으로 std 를 붙여가면서 할 것을 미리 명시하는 방식으로 편의를 위해 존재한다.
+using namespace std;
+
+
+
+int main()
+{
+	// 아래 세 줄은 입력과 출력의 시간을 줄여주기위해 존재한다.
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	cout << fixed;
+	cout.precision(2);
+
+	for (int i = 0; i < tc; i++) {
+		double a;
+		cin >> a;
+		double ans = round(a * 80) / 100;
+		cout << "$" << ans << endl;
+	}
+
+	return 0;
+}
